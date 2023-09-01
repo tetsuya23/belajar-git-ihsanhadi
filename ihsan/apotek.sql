b744d1128af44be41a2b0bd92a2a9b0cdd311582
eshadi <ihsanhadi77@gmail.com>
2023-09-01 17:35:50 +0700

    menambahkan beberapa relasi

--- ihsan/apotek.sql
+++ ihsan/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 28, 2023 at 11:49 AM
+-- Generation Time: Sep 01, 2023 at 12:31 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `apotek`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_merek`
+--
+
+CREATE TABLE `detail_merek` (
+  `id` int(11) NOT NULL,
+  `alamat_produksi` text DEFAULT NULL,
+  `email` varchar(64) NOT NULL,
+  `nomor_hp` int(11) DEFAULT NULL,
+  `id_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -65,9 +92,50 @@ CREATE TABLE `obat` (
 INSERT INTO `obat` (`id`, `nama obat`, `keterangan obat`, `dimunim`, `kadarluarsa`, `id_obat`) VALUES
 (1, 'bejo', 'obat untuk masuk angin', '2x1hari', 101024, 2);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id` int(11) NOT NULL,
+  `list_pembelian` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `diminum` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `tanggal_pembelian` date NOT NULL,
+  `list_beli` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `id_pembeli` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `detail_merek`
+--
+ALTER TABLE `detail_merek`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_obat` (`id_obat`);
+
+--
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD KEY `fk_obat` (`id_obat`),
+  ADD KEY `fk_detail_transaksi` (`id_transaksi`);
 
 --
 -- Indexes for table `merek_obat`
@@ -83,8 +151,27 @@ ALTER TABLE `obat`
   ADD KEY `FK_merek` (`id_obat`);
 
 --
+-- Indexes for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_pembeli` (`id_pembeli`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `detail_merek`
+--
+ALTER TABLE `detail_merek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `merek_obat`
@@ -99,14 +186,45 @@ ALTER TABLE `obat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `detail_merek`
+--
+ALTER TABLE `detail_merek`
+  ADD CONSTRAINT `detail_merek_ibfk_1` FOREIGN KEY (`id_obat`) REFERENCES `merek_obat` (`id`);
+
+--
+-- Constraints for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `fk_detail_transaksi` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id`),
+  ADD CONSTRAINT `fk_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`);
 
 --
 -- Constraints for table `obat`
 --
 ALTER TABLE `obat`
   ADD CONSTRAINT `FK_merek` FOREIGN KEY (`id_obat`) REFERENCES `merek_obat` (`id`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `fk_pembeli` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
